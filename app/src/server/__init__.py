063bb825d4748db1783bc9d01b4608bf2df701bd
fukasawat78 <55072916+fukasawat78@users.noreply.github.com>
2020-02-25 00:35:25 +0900

    update

--- app/src/server/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from . import hoge
-
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config/default')
-
-blueprints = [hoge]
-for blueprint in blueprints:
-    app.register_blueprint(blueprint.app)
